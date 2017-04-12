2e580a4f2427b061f5e57511c9d0a67c1b67c253
puneet1feb <puneet1feb@gmail.com>
2017-04-11 20:45:35 -0400

    New changes

--- target/ArtefactLights/static/js/controller/mainController.js
+++
@@ -1,4 +0,0 @@
-myApp.controller('MainController', function($scope) {
-        // create a message to display in our view
-        $scope.message = 'Everyone come and see how good I look!';
-    });
